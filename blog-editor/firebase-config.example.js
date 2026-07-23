b8cd79c7f1565e598233135d7d4d9e552b62ac84
orangemark0808-code <orangemark0808@gmail.com>
2026-07-23 17:06:59 +0900

    feat(blog-editor): add secure sync and journal metadata

---
+++ blog-editor/firebase-config.example.js
@@ -0,0 +1,10 @@
+// この形式のまま firebase-config.local.js へコピーして値を設定してください。
+window.BLOG_EDITOR_FIREBASE_CONFIG = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
